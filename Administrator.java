875e8b43b8fbca4ef9c5667a15567c432368d8f6
Kristi Samara <ksamara22@epoka.edu.al>
2023-12-14 23:51:50 +0100

    update

---
+++ Administrator.java
@@ -0,0 +1,57 @@
+public class Administrator extends Manager {
+    public Administrator (String username, String password) {
+        super(username, password);
+    }
+    @Override
+    public void getBooks(){
+        //lexo file
+    };
+    @Override
+    public void CreateBill(){
+        
+    };
+    @Override
+    public void SaveTransacation(){
+        //hidhi ne file
+    };
+    @Override
+    public void getLibrarian(){
+        //lexo file
+    };
+    @Override
+    public void getTransaction(){
+        //lexo file
+    };
+    @Override
+    public void addBook(){
+        //hidhi ne file
+    };
+    @Override
+    public void QueryWithTransaction(){
+        //(total bills, books sold, total made by a librarian)
+    };
+     public void addLibrarian(){
+        //hidh ne file
+    };
+     public void addManager(){
+        //hidh ne file
+    };
+     public void UpdateManager(){
+     
+    };
+     public void DeleteManager(){
+       
+    };
+       public void UpdateLibrarian(){
+        
+    };
+       public void DeleteLibrarian(){
+       
+    };
+    
+
+
+
+    
+    
+}
