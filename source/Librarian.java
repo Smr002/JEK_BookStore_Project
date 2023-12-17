c9a27f3b3f23dbcaa7554b1e47c13322e9b1f5b1
Kristi Samara <ksamara22@epoka.edu.al>
2023-12-17 14:26:03 +0100

    Make the creat bill methods,read request file,write in request file, make prove this in test file

--- source/Librarian.java
+++ source/Librarian.java
@@ -1,5 +1,8 @@
 package source;
+
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javafx.collections.FXCollections;
@@ -13,12 +16,19 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
-public class Librarian extends User{
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.text.ParseException;
+
+public class Librarian extends User {
 
     public Librarian(String username, String password) {
         super(username, password);
@@ -34,7 +44,6 @@ public class Librarian extends User{
             } else {
                 Stage booksStage = new Stage();
                 booksStage.setTitle("List of Books");
-
 
                 TableView<Book> table = new TableView<>();
 
@@ -61,7 +70,7 @@ public class Librarian extends User{
                 // Set the columns to the table
                 table.getColumns().addAll(isbnColumn, titleColumn, categoryColumn, priceColumn);
 
-                //add the data to the table
+                // add the data to the table
                 table.setItems(FXCollections.observableArrayList(booksList));
 
                 VBox booksLayout = new VBox();
@@ -76,44 +85,57 @@ public class Librarian extends User{
         }
     }
 
-    public void CreateBill(){
-        int orderId;
-        Book book = new Book();
-
-
-        String filePath = "createBill.txt";
-        File file = new File(filePath);
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+ public List<String> readRequests() {
+    List<String> requests = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader("files/request.txt"))) {
+        String header = reader.readLine();
+        String[] columns = header.split(",");
+        String line;
+        while ((line = reader.readLine()) != null) {
+            requests.add(line); 
+            String[] values = line.split(",");
+            String orderRqst = values[0].trim();
+            String isbnT = values[1].trim();
+            System.out.println(orderRqst + " " + isbnT);
         }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return requests;
+}
 
 
-
-      
-
-
-       
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-
-            writer.write("");
-
-            System.out.println("Content has been written to the file: " + filePath);
+    public void createBill() throws ParseException {
+        Book book = new Book();
+        Librarian librarian = new Librarian("librarian1", "password123");
+        List<Book> booksList = book.readBook();
+        List<String> requests = librarian.readRequests();
+    
+        String filePath = "files/createBill.txt";
+        
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath,true))) {
+            for (Book b : booksList) {
+                for (String request : requests) {
+                    String[] columns = request.split(",");
+                    String orderId = columns[0].trim();
+                    String temp = columns[1].trim();
+                    Date date = new Date();
+                    if (b.getISBN().equals(temp)) {
+                        writer.write(orderId+"," + b.getISBN()+"," + b.getTitle()+"," +b.getAuthor()+","+ date+","+b.getSellingPrice() +"\n");
+                     
+                        System.out.println("Content has been written to the file: " + filePath);
+                    }
+                }
+            }
         } catch (IOException e) {
-       
             e.printStackTrace();
         }
+    }
+    
     
 
-
-    };
-    public void SaveTransacation(){
-        //hidhi ne file
-    };
-
+    public void SaveTransaction() {
+        // Implement the method logic here
+    }
 }
--- source/Test.java
+++ source/Test.java
@@ -1,11 +1,18 @@
 package source;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Test {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
         Book book = new Book();
         List<Book> books = book.readBook();
 
@@ -13,9 +20,68 @@ public class Test {
             System.out.println("ISBN: " + b.getISBN());
             System.out.println("Title: " + b.getTitle());
             System.out.println("Category: " + b.getCatogory());
-            System.out.println("PurchasedDate: " + b.getPurchasedDate());    
-    
+            System.out.println("PurchasedDate: " + b.getPurchasedDate());
+
             System.out.println();
         }
+
+       // requestBook();
+        Librarian librarian = new Librarian("librarian1","password123");
+        System.out.println("The request book are readen");
+        librarian.readRequests();
+        System.out.println("The create bill is done");
+        librarian.createBill();
     }
+
+   public static  void requestBook() throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            Book book = new Book();
+            List<Book> booksList = book.readBook();
+
+            System.out.print("Enter your ISBN for the request:");
+            String isbnTemp = sc.nextLine();
+
+          
+            for (Book b : booksList) {
+                if (b.getISBN().equals(isbnTemp)) {
+                    String filePath = "files/Request.txt";
+                    File file = new File(filePath);
+
+            
+                    if (!file.exists()) {
+                        try {
+                            file.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    try (PrintWriter output = new PrintWriter(new FileWriter(file, true))) {
+                        Random orderRqst = new Random();
+                        output.print(orderRqst.nextInt() + ",");
+                        output.println(isbnTemp);
+                        System.out.println("The request is done succesful");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    break; 
+                }
+                    else{
+                    System.out.println("This book doesntr exist");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sc.close(); 
+        }
+    }
+
+
 }
+     
+
