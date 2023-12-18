dbf81d1a532c39251e2832472548a77d956db3eb
Joel1b <bitrijoel@gmail.com>
2023-12-18 21:44:02 +0100

    Users & Permissions

---
+++ source/Users.java
@@ -0,0 +1,23 @@
+package source;
+import java.util.ArrayList;
+import java.util.List;
+public class Users {
+
+    private final List<Permission> permissions;
+
+    public Users() {
+        this.permissions = new ArrayList<>();
+    }
+
+    public void addPermission(Permission permission) {
+        permissions.add(permission);
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public boolean hasPermission(Permission requiredPermission) {
+        return permissions.contains(requiredPermission);
+    }
+}
