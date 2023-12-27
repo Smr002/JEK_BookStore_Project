f4acef3b74d1afc69fa77e08ffa9557b9c71fa60
Ermin Lilaj <ermin.lilaj04@gmail.com>
2023-12-27 20:38:12 +0100

    -fixed the cart area -added delete from cart opsion -proceed to order visible only when added to cart -created a scene to confirm order

--- source/View/FirstWindow.java
+++ source/View/FirstWindow.java
@@ -20,12 +20,18 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import source.Controller.Methods;
+import source.Main.Main;
 import source.Model.Book;
+import javafx.scene.Node;
 
 public class FirstWindow {
-    static double total = 0;
+private ScrollPane sx;//sx- scrollpane i cart
     private Label cartLabel = new Label("Cart is Empty");
     private VBox allBooksVBox = new VBox(40);
+    private VBox cartVBox = new VBox(10); // New VBox for cart items
+    private Scene orderConfirmationScene;
+
+    private Label totalPriceLabel = new Label("Total Price: $0.00");
 
     public void showFirstWindow() {
         Stage primaryStage = new Stage();
@@ -33,7 +39,7 @@ public class FirstWindow {
 
         BorderPane borderPane = new BorderPane();
         borderPane.setStyle("-fx-border-color: red");
-        Scene scene = new Scene(borderPane, 1000, 850);
+        Scene scene = new Scene(borderPane, 800, 650);
 
         Button rightButton = new Button("Login");
         GridPane labelGrid = new GridPane();
@@ -42,12 +48,12 @@ public class FirstWindow {
         borderPane.setTop(labelGrid);
         gp2.add(rightButton, 0, 0);
         labelGrid.add(createTopLabel(), 0, 0);
-        labelGrid.setAlignment(Pos.TOP_LEFT);
+        //labelGrid.setAlignment(Pos.TOP_LEFT);
 
         List<Book> books = Methods.readBook();
 
         int booksPerRow = 3;
-        // creating bookrow with 3 books per roww
+        // creating bookrow with 3 books per row
         for (int i = 0; i < books.size(); i += booksPerRow) {
             int endIndex = Math.min(i + booksPerRow, books.size());
             List<Book> rowBooks = books.subList(i, endIndex);
@@ -60,21 +66,39 @@ public class FirstWindow {
 
         ScrollPane scrollPane = new ScrollPane(allBooksVBox);
         scrollPane.setPrefViewportWidth(200);
-        ScrollPane sx = new ScrollPane();// kjo eshte per pj add to cart
+       sx = new ScrollPane();// kjo eshte per pj add to cart
         sx.setPrefViewportWidth(300);
 
         borderPane.setCenter(scrollPane);
         borderPane.setLeft(sx);
 
-        sx.setContent(cartLabel);
+
+        sx.setContent(cartVBox);  // Set the content of the new cartVBox
+
+        // Creating the Proceed to order button
+        Button orderButton = new Button("Proceed to order");
+        VBox orderButtonVBox = new VBox(orderButton);
+        orderButtonVBox.setVisible(false);//to make it non visible when cart is empty
+        // Add the orderButtonVBox to the end of sx
+        sx.setContent(new VBox(cartVBox, orderButtonVBox));
 
         rightButton.setOnAction(e -> {
             LoginScene.showLoginScene(primaryStage);
+        });
+        /////////////
+        // Create order confirmation scene
+        orderConfirmationScene = createOrderConfirmationScene(primaryStage);
+
+        orderButton.setOnAction(e -> {
+            // Show the order confirmation scene when proceed to order is clicked
+            primaryStage.setScene(orderConfirmationScene);
         });
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+
 
     private Label createTopLabel() {
         Label labelTop = new Label("JEK-BOOKSTORE");
@@ -144,22 +168,43 @@ public class FirstWindow {
         return bookImageView;
     }
 
+    // Create a delete button to delete certain book in the cart
     private void handleAddToCart(Book book, TextField quantityTextField) {
         String quantityText = quantityTextField.getText();
-        int qua = Integer.parseInt(quantityText);
-
 
         if (isValidQuantity(quantityText)) {
-            String currentText = cartLabel.getText();
-            total+=qua*book.getSellingPrice();
-            String newText = currentText + "\nAdded to Cart " + "\nTitle:" + book.getTitle() + "\nPrice per book:"
-                    + book.getSellingPrice() +
-                    "\nQuantity: " + quantityText + "\nTotal Price for Book: " + qua*book.getSellingPrice() + "\nCurrent Price for all: " + total;
+            HBox cartItemBox = createCartItem(book, quantityText);
+            cartVBox.getChildren().add(cartItemBox);
 
-            cartLabel.setText(newText);
+            VBox orderButtonVBox = (VBox) ((VBox)sx.getContent()).getChildren().get(1);//e ben visible orderButton kur behet add to cart
+            orderButtonVBox.setVisible(true);
         } else {
             showAlert("Invalid Quantity", "Please enter a valid positive integer for quantity.");
         }
+    }
+
+    private HBox createCartItem(Book book, String quantityText) {
+        Label cartItemLabel = new Label("Added to Cart " + "\nTitle:" + book.getTitle() + "\nPrice:"
+                + book.getSellingPrice() +
+                "\nQuantity: " + quantityText + "\n-----------------------------------");
+
+        Button deleteButton = new Button("Remove from cart");
+        deleteButton.setStyle("-fx-background-color: #ff3333"); // Red color for delete button
+        deleteButton.setStyle("-fx-background-radius: 6");
+
+        HBox cartItemBox = new HBox(10);
+        cartItemBox.getChildren().addAll(cartItemLabel,deleteButton);
+        deleteButton.setAlignment(Pos.BOTTOM_RIGHT);
+        cartItemBox.setAlignment(Pos.CENTER_LEFT);
+
+        // Pass cartItemBox to handleDeleteFromCart method
+        deleteButton.setOnAction(e -> handleDeleteFromCart(cartItemBox));
+
+        return cartItemBox;
+    }
+
+    private void handleDeleteFromCart(HBox cartItemBox) {
+        cartVBox.getChildren().remove(cartItemBox);//heq nga cart
     }
 
     private boolean isValidQuantity(String quantityText) {
@@ -178,5 +223,60 @@ public class FirstWindow {
         alert.setContentText(content);
         alert.showAndWait();
     }
-}
+    //////////////////////////////////////////////////
+    private Scene createOrderConfirmationScene(Stage primaryStage) {
+        // Create a GridPane for the order confirmation scene
+        GridPane orderConfirmationGrid = new GridPane();
+        orderConfirmationGrid.setAlignment(Pos.TOP_LEFT);
+        orderConfirmationGrid.setVgap(20);
 
+        Label orderLabel = new Label("Please fill in to order");
+        orderLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        orderLabel.setStyle("-fx-text-fill: green;");
+        orderConfirmationGrid.add(orderLabel, 0, 0);
+        orderConfirmationGrid.add(new Label(" Enter your name:"), 0, 1);
+        TextField nameField = new TextField();
+        nameField.setPromptText("Name");
+        orderConfirmationGrid.add(nameField, 1, 1);
+
+        orderConfirmationGrid.add(new Label(" Enter your surname:"), 0, 2);
+        TextField surnameField = new TextField();
+        surnameField.setPromptText("Surname");
+        orderConfirmationGrid.add(surnameField, 1, 2);
+
+        orderConfirmationGrid.add(new Label(" Enter your email:"), 0, 3);
+        TextField emailField = new TextField();
+        emailField.setPromptText("email");
+        orderConfirmationGrid.add(emailField, 1, 3);
+
+        orderConfirmationGrid.add(new Label(" Enter your phone number:"), 0, 4);
+        TextField phoneNumberField = new TextField();
+        phoneNumberField.setPromptText("Phone number");
+        orderConfirmationGrid.add(phoneNumberField, 1, 4);
+
+        Label totalPricewV= new Label("Price without VAT: ");
+        orderConfirmationGrid.add(totalPricewV,0,5);
+Label vatPrice = new Label("VAT: ");
+orderConfirmationGrid.add(vatPrice,0,6);
+Label totalPrice = new Label("Total: ");
+orderConfirmationGrid.add(totalPrice,0,7);
+//create a confirmation order button
+        Button confirmOrder= new Button("Confirm Order");
+
+        // createe a back button to return to the main scene
+        Button backButton = new Button("Back");
+        backButton.setOnAction(e -> {
+            Main.showMainScene(primaryStage);
+            primaryStage.close();
+        });
+
+
+        orderConfirmationGrid.add(backButton, 0,10);
+orderConfirmationGrid.add(confirmOrder,1,10);
+        return new Scene(orderConfirmationGrid, 450, 450);
+    }
+
+
+
+
+}
