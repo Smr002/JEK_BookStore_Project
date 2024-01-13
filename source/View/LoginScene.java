edcce41bf2f6cadef4ff44ac0a1e8c08e26b5a07
Kristi Samara <ksamara22@epoka.edu.al>
2024-01-13 17:51:38 +0100

    put the logo image in login scene

--- source/View/LoginScene.java
+++ source/View/LoginScene.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import source.Model.User;
@@ -12,29 +13,30 @@ import source.Main.Main;
 public class LoginScene {
     public static void showLoginScene(Stage primaryStage) {
         GridPane grid2 = new GridPane();
-        // Label typeLabel = new Label();
-        Scene scene1 = new Scene(grid2, 800, 700);
-
         GridPane grid = new GridPane();
-        grid.setAlignment(javafx.geometry.Pos.CENTER);
+        grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
+        ImageView logo = new ImageView("images/jek_logo.png");
+        logo.setFitWidth(200);
+        logo.setPreserveRatio(true);
+        grid.add(logo, 0, 0, 2, 1);
         Label usernameLabel = new Label("Username:");
-        grid.add(usernameLabel, 0, 0);
+        grid.add(usernameLabel, 0, 1);
         TextField usernameTextField = new TextField();
-        grid.add(usernameTextField, 1, 0);
+        grid.add(usernameTextField, 1, 1);
 
         Label passwordLabel = new Label("Password:");
-        grid.add(passwordLabel, 0, 1);
+        grid.add(passwordLabel, 0, 2);
         PasswordField passwordField = new PasswordField();
-        grid.add(passwordField, 1, 1);
+        grid.add(passwordField, 1, 2);
 
         Button loginButton = new Button("Login");
-        grid.add(loginButton, 2, 1);
+        grid.add(loginButton, 0, 3);
         Button backButton = new Button("Back");
-        grid.add(backButton, 1, 2);
+        grid.add(backButton, 1, 3);
 
         Label typeLabel1 = new Label();
         grid.add(typeLabel1, 0, 0);
