c680e71e7eaca24daca6bd148d3b456860d85631
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-01-14 10:58:15 +0100

    login updatee

--- source/View/LoginScene.java
+++ source/View/LoginScene.java
@@ -12,7 +12,34 @@ import source.Main.Main;
 
 public class LoginScene {
     public static void showLoginScene(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setStyle("-fx-background-color: linear-gradient(to bottom, #ADD8E6, #87CEEB);");
 
+        ImageView logo = new ImageView("images/jek_logo.png");
+        logo.setFitWidth(200);
+        logo.setPreserveRatio(true);
+        grid.add(logo, 0, 0, 2, 1);
+        Label usernameLabel = new Label("Username:");
+        grid.add(usernameLabel, 0, 1);
+        TextField usernameTextField = new TextField();
+        grid.add(usernameTextField, 1, 1);
+
+        Label passwordLabel = new Label("Password:");
+        grid.add(passwordLabel, 0, 2);
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+
+        Button loginButton = new Button("Login");
+        grid.add(loginButton, 0, 3);
+        Button backButton = new Button("Back");
+        grid.add(backButton, 1, 3);
+
+
+        Label typeLabel1 = new Label();
         grid.add(typeLabel1, 0, 0);
 
         loginButton.setOnAction(e -> {
